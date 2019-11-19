9d3becbefae5453f084fa674961d61a6a12e712c
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-11-20 00:38:36 +0900

    8-2-1-4

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,18 @@
+document.addEventListener("turbolinks:load", function() {
+  document.querySelectorAll("td").forEach(function(td) {
+    td.addEventListener("mouseover", function(e) {
+      e.currentTarget.style.backgroundColor = "";
+    });
+
+    td.addEventListener("mouseout", function(e) {
+      e.currentTarget.style.backgroundColor = "";
+    });
+  });
+  document.querySelectorAll(".delete").forEach(function(a) {
+    a.addEventListener("ajax:success", function() {
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = "none";
+    });
+  });
+});
